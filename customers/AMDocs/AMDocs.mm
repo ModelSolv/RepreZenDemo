ecb5f46dada1c85ccd603a4746489840b7c42d1a
Miles Daffin <miles.daffin@gmail.com>
2016-08-31 14:39:12 +0100

    Creating demo project for AMDocs meeting demo.

---
+++ customers/AMDocs/AMDocs.mm
@@ -0,0 +1,75 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="AMDocs" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1472562404679"><hook NAME="MapStyle" zoom="1.464">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2"/>
+<node TEXT="API Management Platforms" POSITION="right" ID="ID_1725061049" CREATED="1472562408030" MODIFIED="1472562415958">
+<edge COLOR="#ff0000"/>
+<node TEXT="APIGee" ID="ID_1018824068" CREATED="1472562421856" MODIFIED="1472562425498"/>
+<node TEXT="Mashery" ID="ID_1538091161" CREATED="1472562430093" MODIFIED="1472562432219"/>
+<node TEXT="IBM" ID="ID_253073957" CREATED="1472562425955" MODIFIED="1472562429772"/>
+</node>
+<node TEXT="Agenda" POSITION="right" ID="ID_526339214" CREATED="1472562514127" MODIFIED="1472562564232">
+<edge COLOR="#0000ff"/>
+<node TEXT="How does RepreZen API Studio differ from an API management platform?" ID="ID_1047139372" CREATED="1472562575589" MODIFIED="1472562585212">
+<node TEXT="APIGee" ID="ID_747879814" CREATED="1472562777530" MODIFIED="1472562780980"/>
+<node TEXT="IBM" ID="ID_1005112501" CREATED="1472562781264" MODIFIED="1472562782623"/>
+<node TEXT="Mashery" ID="ID_725506763" CREATED="1472562782940" MODIFIED="1472562784891"/>
+</node>
+<node TEXT="How does it complement &apos;traditional&apos; APIM platforms?" ID="ID_1107919404" CREATED="1472562585926" MODIFIED="1472562616059">
+<node TEXT="If so, in what way?" ID="ID_1178630967" CREATED="1472562627396" MODIFIED="1472562642835"/>
+</node>
+<node TEXT=" What would I be benifiting if I use this side by side to APIGee?" ID="ID_1327481004" CREATED="1472562656602" MODIFIED="1472562681535">
+<node TEXT="is it a must or a nice to have?" ID="ID_108211283" CREATED="1472562681915" MODIFIED="1472562681915"/>
+</node>
+<node TEXT="How does RepreZen API Studio add value to the overall task of creating and managing APIs (compared with other stuff that may be cheaper or free)?" ID="ID_1513640316" CREATED="1472562575589" MODIFIED="1472562575589"/>
+</node>
+</node>
+</map>
